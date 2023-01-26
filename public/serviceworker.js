b1cfe5d9f5dc465375eabddd31f03bb6a197db0e
Daniel Doyle <daniel@Daniels-MacBook-Pro-2.local>
2023-01-26 09:10:17 +0000

    Initial commit

---
+++ public/serviceworker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = [ "index.hmtl", "offline.html" ];
+
+const self = this;
+
+// Installation of SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) // if we cannot fetch because not everything has been cached and there is no internet, we display offline.html
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) { // keep only the specific files from cache versions we need
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import './App.scss'; // need to run 'npm install sass'
+
+// import components
+import Selection from "./components/Selection";
+import Playlist from "./components/Playlist";
+
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+const createCollectionsInIndexedDB = () => {
+    if (!indexedDB) {
+        console.log("IndexedDB could not be found in this browser.");
+    }
+
+    console.log(indexedDB);
+
+    // if a database doesn not already exist with name "ExerciseDatabase" one is created, second param a version number in case we make a change and the user already has a previous version stored in broowser
+    const request = indexedDB.open("ExerciseDatabase", 1);
+
+    // error checks before we create schema on database
+    request.onerror = function (event) {
+        console.error("An error occurred with IndexedDB");
+        console.error(event);
+    };
+
+    // create schema of database
+    request.onupgradeneeded = function () {
+        const db = request.result; // db is the result of the open request, which is the exercise database
+        if(!db.objectStoreNames.contains("ExerciseDatabase")) {
+            const store = db.createObjectStore("exercises", { keyPath: "id" }); // creating object store eqquivalent to creating tables in SQL
+            store.createIndex("muscle_idx", ["muscle"], { unique: false }); // compound index to search for exercises by difficulty and muscle grouup
+            // above we specify that it is not unique as we can have exercises with the same  difficulty and muscles trained
+        }
+    };
+
+    request.onsuccess = () => {
+        const db = request.result;
+        const transaction = db.transaction("exercises", "readwrite");
+        const store = transaction.objectStore("exercises");
+        store.put({ id: 1, name: "squat", muscle: "glute", difficulty: "easy" });
+        transaction.oncomplete = function () {
+            db.close();
+        };
+        console.log('Database loaded successfully!');
+    }
+}
+
+const App = () => {
+    useEffect(() => {
+        createCollectionsInIndexedDB();
+    }, []);
+    return (
+        <div className="App">
+            <h1>TRX Star (navbar)</h1>
+            <Routes>
+                <Route path="/playlist" element={ <Playlist indexedDB={indexedDB}/> } />
+                <Route path="/" element={ <Selection/> } />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
