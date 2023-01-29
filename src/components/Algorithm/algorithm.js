8f35e59e577cda063c575bb6e48809f5b943bd7b
Sofia Hernandez Gelado <sofiahgelado@gmail.com>
2023-01-29 18:29:02 +0000

    filterDatabase function OK, allObjects bug

--- src/components/Algorithm/algorithm.js
+++ src/components/Algorithm/algorithm.js
@@ -1,33 +1,62 @@
-function recommendationAlgorithm(_difficulty, _focus, _time, _muscleGroup, _muscleType)
-{
+import React, { useEffect, useState } from 'react';
+
+
+export function recommendationAlgorithm(_difficulty, _focus, _time, _muscleGroup, _muscleType) {
     // fetch valid exercises from database
     var dictPreferences = {
         "difficulty": _difficulty,
         "focus": _focus,
-        "time": _time, 
-        "muscleGroup" : _muscleGroup,
+        "time": _time,
+        "muscleGroup": _muscleGroup,
         "muscleType": _muscleType
-      };   
+    };
+
+    return filterDatabase(dictPreferences);
+
+
 }
 
-function filterDatabase(dictPreferences)
-{
-    let request = indexedDB.open("ExerciseDatabase", 1);
-    request.onsuccess = function(event) {
-      let db = event.target.result;
-      let objectStore = db.transaction("FilteredExercises").objectStore("FilteredExercises");
-      let request = objectStore.getAll();
-      request.onsuccess = function(event) {
-        let data = event.target.result;
+export function filterDatabase (tableName, indexNameArray, valueArray, indexedDB, stateCb)
+/*
 
-        for (const [key, value] of Object.entries(dictPreferences))
-        {
-            if (value == None)
-                continue; 
+tableName: string
+*/ {
+    const dbPromise = indexedDB.open("ExerciseDatabase", 1);
+    dbPromise.onsuccess = () => {
 
-            let filteredData = data.filter(item => item.key === value)
-            console.log(filteredData);
+        const db = dbPromise.result;
+        const transaction = db.transaction([tableName], "readonly");
+        const store = transaction.objectStore(tableName);
+
+        var request = store.getAll(); 
+        // Get all objects from the object store
+        request.onsuccess = function (event) {
+            var allObjects = event.target.result;
+            console.log("All objects:", allObjects);
+
+            if (indexNameArray.length != valueArray.length) {
+                console.log.error("Invalid filtering inputs arrays");
+                return;
+            }
+
+            for (let i = 0; i < indexNameArray.length; i++) {
+                if (typeof (valueArray[i]) === "number") {
+                    // Filter the objects based on two attributes
+                    allObjects = allObjects.filter(object => object.attribute1 === valueArray[i]);
+                }
+                else if (typeof (valueArray[i] === "string")) {
+                    // Filter the objects that have the desired string in the array
+                    allObjects = allObjects.filter(object => object.muscle_type.includes(valueArray[i]));
+                }
+
+            }
+
+        
+
+        console.log("Filtered:", allObjects);
+        stateCb(allObjects[0]);
         }
-      };
-    };
+
+    }
+
 }
