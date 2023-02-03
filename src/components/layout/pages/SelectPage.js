050cd6a9c803304c50ef18f523070200494fb790
Adela Viskova <av1322@imperial.ac.uk>
2023-02-03 21:10:59 +0000

    Progress bar alighned to the top

--- src/components/layout/pages/SelectPage.js
+++ src/components/layout/pages/SelectPage.js
@@ -14,21 +14,25 @@ const style = {
 
 const SelectPage = ({ selectForm }) => {
     return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                width: "100%",
-                height: "100vh",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <SelectProgress/>
-            {selectForm}
-        </div>
+        <>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "100%",
+                    height: "100%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <SelectProgress/>
+                
+            </div>
+            <div>
+                {selectForm}
+            </div>
+        </>
     )
 }
 
