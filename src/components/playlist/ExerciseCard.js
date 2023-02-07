0a641052617ee3deb64b80649aba26de3fa55032
Daniel Doyle <daniel@Daniels-MacBook-Pro-2.local>
2023-02-07 08:27:30 +0000

    draft playlist introduced

---
+++ src/components/playlist/ExerciseCard.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { FiChevronDown } from 'react-icons/fi'
+import { BsThreeDots } from 'react-icons/bs'
+import { RiTimerLine } from 'react-icons/ri'
+import './index.scss'
+
+const ExerciseCard = () => {
+    const [open, setOPen] = useState(false);
+
+    const toggle = () => {
+        setOPen(!open);
+      };
+
+    return (
+        <>
+            <div className='exercise-card'>
+                <div className='exercise-card__left-container'>
+                    <FiChevronDown onClick={toggle} size={28} className='exercise-card__chevron'/>
+                    <p>Exercise name</p>
+                </div>
+                <BsThreeDots size={28} className='exercise-card__three-dots'/>
+            </div>
+            {open && <div>additional exercise info</div>}
+            <div className='rest-card'>
+                <RiTimerLine size={28} />
+                <p className='rest-card__text'>30s rest</p>
+            </div>
+        </>
+    );
+}
+
+export default ExerciseCard;
