5a3e81693fbf82dbd91bacf13b1fd1f5206c6b97
Sofia Hernandez Gelado <sofiahgelado@gmail.com>
2023-02-14 09:31:21 +0000

    timer implemented -bug counts to negative numbers

--- src/components/playlist/RestPage.js
+++ src/components/playlist/RestPage.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 import { DIFFICULTY } from "../utils/constants";
 import playlist_to_clipList from './playlist_to_clipList';
+import Timer from './Timer';
 
 const RestPage = () => {
     const playlist = useSelector((state) => (state.playlist));
@@ -11,6 +12,9 @@ const RestPage = () => {
     return (
         <>
             <h1>Rest page</h1>
+            <Timer />
+            
+
         </>
 
     )
