6fd2dd196987568fdb5439b343328a806157732f
Eugene Ting <eugeneting@ymail.com>
2023-02-15 17:59:59 +0000

    Made youtube player responsice.

--- src/components/playlist/YouTubePage.js
+++ src/components/playlist/YouTubePage.js
@@ -1,30 +1,42 @@
+import { useState, useEffect } from 'react';
 import YouTube from 'react-youtube';
 import BasicButton from './BasicButton';
 
+
 const YouTubePage = ({nextVideo, exerciseData}) => {
       
-      const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-          start: exerciseData.startTime, 
-          end: exerciseData.endTime,
-        },
-      };
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const opts = {
+    height: width * 9 / 16 * 0.7,
+    width: width * 0.7,
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      start: exerciseData.startTime, 
+      end: exerciseData.endTime,
+    },
+  };
     
-      return (
-        <>
-          <div className="App">
-            <YouTube videoId={exerciseData.videoId} opts={opts} onEnd={nextVideo}/>
-          </div>  
-          <BasicButton
-            option={"Skip to next exercise"}
-            next={nextVideo}
-          />
-        </>
-      );
+  return (
+    <>
+      <div
+        align="center" 
+        className='video'>
+        <YouTube videoId={exerciseData.videoId} opts={opts} onEnd={nextVideo}/>
+      </div>  
+      <BasicButton
+        option={"Skip to next exercise"}
+        next={nextVideo}
+      />
+    </>
+  );
       
 }
 
