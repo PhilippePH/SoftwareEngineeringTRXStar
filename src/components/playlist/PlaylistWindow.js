b1eaf3572ddb0bae33e1eb2797fcd0a4badcacc7
Soren Antebi <sorenantebi@Sorens-MacBook-Pro.local>
2023-02-17 15:47:35 +0000

    touched up PlaylistWindow code

--- src/components/playlist/PlaylistWindow.js
+++ src/components/playlist/PlaylistWindow.js
@@ -19,21 +19,19 @@ const PlaylistWindow = () => {
 
     return (
         <div>
-        <div className='playlist-window'>
-               
-            <div className='playlist-window__container'>
-                <div className='workout-heading'>WORKOUT 1</div>
-                <div>
-                    {
-                        playlist.map((work, index) => {
-                            if (playlist[index].type == "cooldown")
-                            return (
-                                <ExerciseCard exercise_name='Cooldown' />
-                            )
-                            while ((index+1) < playlist.length) {
-                                if (playlist[index+1].type == "rest"){
+            <div className='playlist-window'>
+
+                <div className='playlist-window__container'>
+                    <div className='workout-heading'>WORKOUT 1</div>
+                    <div>
+                        {
+                            playlist.map((work, index) => {
+                                if (index + 1 < playlist.length && playlist[index + 1].type == "rest") {
                                     return (<ExerciseCard exercise_name={playlist[index].exercise_name} />)
                                 }
+
+
+
                                 else if (playlist[index].type == "exercise") {
                                     return (
                                         <div>
@@ -49,38 +47,41 @@ const PlaylistWindow = () => {
 
                                         <ExerciseCard exercise_name='Warmup' />
                                     )
-                                
+                                else if (playlist[index].type == "cooldown")
+                                    return (
+                                        <ExerciseCard exercise_name='Cooldown' />
+                                    )
                                 else
                                     return (
                                         <RestCard time={playlist[index].time} />
                                     )
-                            }
-                        })
 
-                    }
+                            })
+
+                        }
+                    </div>
                 </div>
+
             </div>
-            
-        </div>
-        <div
-            style={{
-                display:"flex",
-                placeItems: "center",
-                justifyContent: "center",
-                paddingTop:"2%",
-                alignItems: "center",
-            }}>
-            <FaPlay 
+            <div
                 style={{
-                    // borderRadius: "8px"
-                    fontSize:"50px",
-                    cursor: "pointer"
-                }}
-                onClick={() => { navigate("/youtube") }}
+                    display: "flex",
+                    placeItems: "center",
+                    justifyContent: "center",
+                    paddingTop: "2%",
+                    alignItems: "center",
+                }}>
+                <FaPlay
+                    style={{
+                        // borderRadius: "8px"
+                        fontSize: "50px",
+                        cursor: "pointer"
+                    }}
+                    onClick={() => { navigate("/youtube") }}
                 />
-        </div>
-        { <NavButtons
-                next="/youtube" /> }
+            </div>
+            {<NavButtons
+                next="/youtube" />}
         </div>
     );
 
