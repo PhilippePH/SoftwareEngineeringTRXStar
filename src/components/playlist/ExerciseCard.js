4e40c1b936186b5a128008f8e0444ece913e3bde
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-02-19 01:16:21 +0000

    the text at the bottom isn't quite there yet, but accordion is working.

--- src/components/playlist/ExerciseCard.js
+++ src/components/playlist/ExerciseCard.js
@@ -1,40 +1,67 @@
 import React, { useState } from 'react';
-import { FiChevronDown } from 'react-icons/fi'
-import { IoReload } from 'react-icons/io5'
-import { IoTrashOutline } from 'react-icons/io5'
-import './ExerciseCard.scss'
+import { FiChevronDown } from 'react-icons/fi';
+import { IoReload } from 'react-icons/io5';
+import { IoTrashOutline } from 'react-icons/io5';
+import './ExerciseCard.scss';
+import cn from "classnames";
+import {useRef} from 'react';
 
-const ExerciseCard = ({exercise_name}) => {
-    const [open, setOPen] = useState(false);
+const CLOSED_HEIGHT = 50;
+const OPENED_HEIGHT = 100;
+
+export default function ExerciseCard({exercise_name}) {
+    const [isOpen, setOPen] = useState(false);
+    const outerHeight = useRef(CLOSED_HEIGHT);
+    const containerRef = useRef(null);
+
+    
+    const toggle = () => {
+        if (!isOpen) {
+            outerHeight.current = OPENED_HEIGHT;
+        }
+        setOPen(!isOpen);
+    };
 
     const handleIconClick = (event) => {
         // Handle icon click event
         event.stopPropagation();
-
-      }
-    const toggle = () => {
-        setOPen(!open);
-      };
+    }
 
     return (
         <div style={{paddingBottom: "0.5rem"}}>
-            <div className='exercise-card' onClick={toggle}>
-                <div className='exercise-card__left-container'>
-                    <FiChevronDown  size={28} className={`exercise-card__chevron  exercise-card__chevron${open ? "__open" : "__closed"}`}/>
-                    <p>{exercise_name}</p>
+        <div className='container'>
+            <form 
+                onClick={toggle}
+                ref = {containerRef}
+                className = {cn("exercise-card", {
+                    open: isOpen,
+                    closed: !isOpen
+                })}
+                style={{
+                    minHeight: isOpen ? outerHeight.current : CLOSED_HEIGHT,
+                    width:"100%"
+                }}    
+            >   
+            <div className='exercise-card__left-container'>
+                <FiChevronDown  size={28} className={`exercise-card__chevron  exercise-card__chevron${isOpen ? "__open" : "__closed"}`}/>
+                <div className='exercise-card__exercise-name'>
+                    <p>{exercise_name} </p>
+
+                    <div className='exercise-card__additional-info'>
+                        <p> hey  </p>
+                    </div>
                 </div>
-                <div className='exercise-card__right-container' onClick={handleIconClick}>
-                <IoReload size={28} className='exercise-card__reload'/>
-                <IoTrashOutline size={28} className='exercise-card__trash'/>
                 
-                </div>
             </div>
-            {open && <div>additional exercise info</div>}
-           
             
+            <div className='exercise-card__right-container' onClick={handleIconClick}>
+                <IoReload size={28} className='exercise-card__reload'/>
+                <IoTrashOutline size={28} className='exercise-card__trash'/> 
+            </div>
+
+            
+            </form>
+        </div>
         </div>
     );
 }
-
-
-export default ExerciseCard;
