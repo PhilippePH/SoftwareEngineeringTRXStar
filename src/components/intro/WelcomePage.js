a3b25c30c8997d28bde0ee5d414d6edab2cdbfef
Daniel Doyle <daniel@Daniels-MacBook-Pro-2.local>
2023-02-19 20:57:39 +0000

    Bug fix on welcome page for iPhone and refactor css to extract inline styling

--- src/components/intro/WelcomePage.js
+++ src/components/intro/WelcomePage.js
@@ -26,47 +26,21 @@ const Welcome = () => {
       }, []);
 
     return (
-        <div style={{ display: 'grid', height: '90vh', justifyContent: 'center', placeItems: "center", alignItems: "center"}}>
+        <div className="welcome__img-container">
             <ul
-            style={{
-                display: "grid",
-                //flexWrap:"wrap",
-                placeItems: "center",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "0px", 
-                //maxWidth: width > 768 ? "500px":"350px",
-                // gridTemplateColumns: width > 768 ? 'repeat(4, 1fr)' : 'repeat(2, 1fr)',
-            }}>
+            className="welcome__img-ul" 
+            >
                 <img
-                    style={{
-                        paddingLeft: 15,
-                        width: width > 768 ? "15rem": width < 500? "10rem": "12rem",
-                        height: width > 768 ? "15rem":width < 500? "10rem": "12rem",
-
-                        paddingBottom:"5rem"
-                    }}
+                    className="welcome__img"
                     src={logo}
                     alt={"logo"}
                 />
            
-                <button className='custom-button'
+                <button 
                     key={Welcome} 
                     onClick={()=>clickHandler()}
-                    style={{
-                        width: width > 768 ? "320px" : width < 500? "200px" : "260px",
-                        height: width > 768 ? "6rem": width < 500? "3rem": "4rem",
-                        fontSize: width > 768 ? "2rem": width < 500? "1.2rem":"1.5rem",                        paddingTop: "0.5rem",
-                        paddingBottom: "0.5rem",
-                        paddingLeft: "1rem",
-                        paddingRight: "1rem",
-                        margin: "1rem",
-                        textAlign: "center",
-                        cursor: "pointer",
-                        border: "3px solid black",
-                        borderRadius: "20px",
-                     
-                    }}>
+                    className="welcome__button"
+                    >
                     Start Your Workout
                 </button>
                 
