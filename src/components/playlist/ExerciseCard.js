ea069d9f6581f5d29d68d672da045632bfd96d62
Soren Antebi <sorenantebi@dyn3190-217.wlan.ic.ac.uk>
2023-02-20 13:09:07 +0000

    Accordion has 2 divs

--- src/components/playlist/ExerciseCard.js
+++ src/components/playlist/ExerciseCard.js
@@ -4,17 +4,17 @@ import { IoReload } from 'react-icons/io5';
 import { IoTrashOutline } from 'react-icons/io5';
 import './ExerciseCard.scss';
 import cn from "classnames";
-import {useRef} from 'react';
+import { useRef } from 'react';
 
 const CLOSED_HEIGHT = 50;
 const OPENED_HEIGHT = 100;
 
-export default function ExerciseCard({exercise_name, duration, sets, intensity}) {
+export default function ExerciseCard({ exercise_name, duration, sets, intensity }) {
     const [isOpen, setOPen] = useState(false);
     const outerHeight = useRef(CLOSED_HEIGHT);
     const containerRef = useRef(null);
 
-    
+
     const toggle = () => {
         if (!isOpen) {
             outerHeight.current = OPENED_HEIGHT;
@@ -28,47 +28,52 @@ export default function ExerciseCard({exercise_name, duration, sets, intensity})
     }
 
     return (
-        <div style={{paddingBottom: "0.5rem"}}>
-        <div className='container'>
-            <form 
-                onClick={toggle}
-                ref = {containerRef}
-                className = {cn("exercise-card", {
-                    open: isOpen,
-                    closed: !isOpen
-                })}
-                style={{
-                    minHeight: isOpen ? outerHeight.current : CLOSED_HEIGHT,
-                    width:"100%"
-                }}    
-            >   
-            <div className='exercise-card__left-container'>
-                <FiChevronDown  size={28} className={`exercise-card__chevron  exercise-card__chevron${isOpen ? "__open" : "__closed"}`}/>
-                <div className='exercise-card__exercise-name'>
-                    <p>{exercise_name} </p>
-
-                    <div className='exercise-card__additional-info'>
+        <div style={{ paddingBottom: "0.5rem" }}>
+            <div     onClick={toggle}
+                    ref={containerRef}
+                    className={cn("custom-container", {
+                        open: isOpen,
+                        closed: !isOpen
+                    })}
+                    style={{
+                        //height:  isOpen ? '20vh' : '10vh',
+                        //minHeight: isOpen ? outerHeight.current-20 : CLOSED_HEIGHT-10,
+                        //maxHeight: isOpen ? outerHeight.current : CLOSED_HEIGHT
+                        minHeight: isOpen ? outerHeight.current : CLOSED_HEIGHT
+                    }}>
+                <div
+                    
+                >
+                <div className='exercise-card'>
+                <FiChevronDown size={28} className={`exercise-card__chevron  exercise-card__chevron${isOpen ? "__open" : "__closed"}`} />
+                    <div className='exercise-card__left-container'>
                        
-                        <span> Duration: {duration}s per set</span>
-                        <br/>
-                        <span> Sets: {sets} </span>
-                        <br/>
-                        {/*<span> Intensity: {intensity} </span>*/}
-                      
-                        
-                    </div>
-                </div>
-                
-            </div>
-            
-            <div className='exercise-card__right-container' onClick={handleIconClick}>
-                <IoReload size={28} className='exercise-card__reload'/>
-                <IoTrashOutline size={28} className='exercise-card__trash'/> 
-            </div>
+                        <div className='exercise-card__exercise-name'>
+                            <p>{exercise_name} </p>
 
-            
-            </form>
-        </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className='exercise-card__right-container' onClick={handleIconClick}>
+                        <IoReload size={28} className='exercise-card__reload' />
+                        <IoTrashOutline size={28} className='exercise-card__trash' />
+                    </div>
+
+
+                </div>
+                </div>
+                <div className='additional-info'>
+
+                    <span> Duration: {duration}s per set</span>
+                    <br />
+                    
+                    {/*<span> Intensity: {intensity} </span>*/}
+
+
+                </div>
+            </div>
         </div>
     );
 }
