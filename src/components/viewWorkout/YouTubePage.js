22198a240348f4bdf93e62bacf7184b0bc522c36
Daniel Doyle <daniel@dyn3223-221.wlan.ic.ac.uk>
2023-03-03 16:29:38 +0000

    implement fast-forward and rewind helper functions

--- src/components/viewWorkout/YouTubePage.js
+++ src/components/viewWorkout/YouTubePage.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import YouTube from 'react-youtube';
 import BasicButton from './BasicButton';
+import { FaPlay } from "react-icons/fa";
 import './YouTubePage.scss'
 import '../utils/style.scss'
 import { useDispatch } from "react-redux";
@@ -14,6 +15,9 @@ const YouTubePage = ({nextVideo, exerciseData}) => {
   const dispatch = useDispatch();
       
   const [width, setWidth] = useState(window.innerWidth);
+
+  const playerRef = useRef(null); // create a ref for the YouTube component
+
   console.log("exercisedata", exerciseData)
 
   useEffect(() => {
@@ -21,6 +25,20 @@ const YouTubePage = ({nextVideo, exerciseData}) => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
+
+  const fastForward = async () => {
+    console.log('fast-forwarding!')
+    const currentTime = await playerRef.current.internalPlayer.getCurrentTime(); 
+    // ADD logic such that we cannot go beyond the bounds of our current video
+    playerRef.current.internalPlayer.seekTo(currentTime + 10, true); 
+  };
+
+  const rewind = async () => {
+    console.log('fast-forwarding!')
+    const currentTime = await playerRef.current.internalPlayer.getCurrentTime(); 
+    // ADD logic such that we cannot go beyond the bounds of our current video
+    playerRef.current.internalPlayer.seekTo(Math.max(0, currentTime - 10), true); 
+  };
 
   const clickHandler = () => {
     dispatch(setNavDirection("forwards"));
@@ -35,9 +53,10 @@ const YouTubePage = ({nextVideo, exerciseData}) => {
       autoplay: 1,
       start: exerciseData.startTime, 
       end: exerciseData.endTime,
+      disablekb: 1,
+      controls: 1, // switch to 0 to not show them
     },
   };
-    
       return (
         <>
         <div className='container-youtube'>
@@ -49,8 +68,12 @@ const YouTubePage = ({nextVideo, exerciseData}) => {
           <div
             align="center"
             className="video">
-            <YouTube videoId={exerciseData.videoId} opts={opts} onEnd={nextVideo}/>
+            <YouTube videoId={exerciseData.videoId} opts={opts} onEnd={nextVideo} ref={playerRef} />
           </div>  
+          <div>
+            <FaPlay onClick={() => rewind()}/>
+          </div>
+  
           <div
             className="div-button">
           <BasicButton
