a0b01147fdba0edb14eda78dcfa12cffcd129e67
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-03-05 15:21:39 +0000

    better style for progress bar

--- src/components/viewWorkout/WorkoutProgress.js
+++ src/components/viewWorkout/WorkoutProgress.js
@@ -1,42 +1,20 @@
-
+import './WorkoutProgress.scss'
 
 
 const WorkoutProgress = (props) => {
   const {completed} = props;
   
-  
-  const containerStyles = {
-    height: 30,
-    width: '100%',
-    backgroundColor: "#e0e0de",
-    borderRadius: 50,
-  }
-
   const fillerStyles = {
-    height: '100%',
     width: `${completed}%`,
-    backgroundColor: "#FFE600",
-    borderRadius: 'inherit',
-    textAlign: 'right',
-    color: 'black',
-    fontWeight: 'bold',
-    lineHeight: 2,
-    verticalAlign: 'middle',
-    paddingRight: 10
   }
   
   return (
-    <div style={containerStyles}>
-      <div style={fillerStyles}>
+    <div className="progBarContainer">
+      <div className="filler" style={fillerStyles}>
        <p> {`${completed}%`} </p>
       </div>
     </div>
   );
 };
-
-
-
-
-
 
 export default WorkoutProgress;
