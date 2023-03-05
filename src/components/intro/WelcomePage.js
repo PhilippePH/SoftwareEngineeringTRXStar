217c2f5d7ffeef13fd72cc2e3238ced324e854b4
Adela Viskova <av1322@imperial.ac.uk>
2023-03-05 15:46:56 +0000

    About button implemented

--- src/components/intro/WelcomePage.js
+++ src/components/intro/WelcomePage.js
@@ -8,6 +8,7 @@ import { setNavDirection } from "../../redux/slices/selectSlice";
 import { store } from "../../redux/store"
 import { initialiseAll } from "../../redux/slices/selectSlice";
 import { initialisePlaylist } from "../../redux/slices/playlistSlice";
+import { OverlayTrigger, Popover } from "react-bootstrap";
 
 const Welcome = () => {
     store.dispatch(initialiseAll());
@@ -43,6 +44,9 @@ const Welcome = () => {
                     >
                     Start Your Workout
                 </button>
+                <OverlayTrigger trigger={['click', 'hover']} placement="bottom" overlay={popover}>
+                    <button className="welcome__aboutButton"> About </button> 
+                </OverlayTrigger>
                 
             </ul>
         </div>
@@ -50,3 +54,12 @@ const Welcome = () => {
 }
 
 export default Welcome;
+
+
+const popover = (
+    <Popover id="popover-basic" className="welcome__aboutPopover">
+      <Popover.Body className='popover-text'>
+        TRX STAR is an exercise app that generates a workout with trx bands based on your specific wants and needs. Simply select your preferences and let us create a curated playlist for you! 
+      </Popover.Body>
+    </Popover>
+);
