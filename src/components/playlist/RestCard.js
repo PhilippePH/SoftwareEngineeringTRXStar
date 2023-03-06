8840216cf578d3fd6cb9c27cacfc24a16bde049f
Sofia Hernandez Gelado <sofiahgelado@gmail.com>
2023-03-06 18:18:16 +0000

    Rest cards individual

--- src/components/playlist/RestCard.js
+++ src/components/playlist/RestCard.js
@@ -1,12 +1,52 @@
 
 import './ExerciseCard.scss'
+import { BsHourglassSplit, BsTrash } from 'react-icons/bs';
+import { useState } from 'react';
+import { store } from '../../redux/store';
+import { removeFromPlaylist } from "../../redux/slices/playlistSlice.js"
 
-const RestCard = ({time}) => {
+
+const RestCard = ({time, index}) => {
+
+    const [isFadingOut, setIsFadingOut] = useState(false);
+
+    const fadeOut = (event) => {
+        setIsFadingOut(true);
+    };
+    
+    const handleRemoveDiv = (event) => {
+        event.stopPropagation();
+        fadeOut(); 
+        setTimeout(() => {
+            store.dispatch(removeFromPlaylist(index));
+            setIsFadingOut(false);
+          }, 1000);
+      };
+
+
     return(
-            <div className='rest-card'>
-              
-                <p className='rest-card__text'>{time}s rest</p>
-            </div>
+        <div className={isFadingOut ? 'item-fadeout' : 'item1'}>
+        <div className={'custom-container'}
+        style = {{minHeight : 50}}>
+                    <div>
+                        <div className={'exercise-card'}>
+                            <div className={'exercise-card__left-container'}>
+                                <BsHourglassSplit size={28} color='gray' />
+                                <div className='exercise-card__exercise-name'>
+                                    {time}s rest
+                                </div>
+                            </div>
+                            <div className='exercise-card__right-container' >
+                                <BsTrash size={28} className='exercise-card__trash' onClick={handleRemoveDiv} style={{ marginRight: '10px', strokeWidth: '0.3' }} />
+                            </div>
+                        </div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </div>
+        </div>
+
            
     );
 }
