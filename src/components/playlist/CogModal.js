4a2e0e8e7c4115e4f9e24c61ba729532793203d4
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-03-11 16:43:50 +0000

    HTML buttons. Styling to be done

--- src/components/playlist/CogModal.js
+++ src/components/playlist/CogModal.js
@@ -1,20 +1,28 @@
 import Modal from 'react-bootstrap/Modal'
 import { useSelector } from 'react-redux'
+import { useEffect } from "react";
 import { updateRest } from '../../redux/slices/playlistSlice';
 import { store } from "../../redux/store"
-
+import SelectButton from "../utils/SelectButton";
+import { useDispatch } from "react-redux";
+import { setActiveTab } from "../../redux/slices/selectSlice";
+import '../selection/style.scss';
+import { Button } from 'react-bootstrap';
 
 const CogModal = ({show, unshow}) => {
 
     async function handleSubmit(e) {
         e.preventDefault();
         const formData = new FormData(e.target);
-        const data = Object.fromEntries(formData.entries())["restTime"];
 
-        const replaceForCardio = 1; // Here replace this for whether replace cardio has been clicked some use state or smth
-        store.dispatch(updateRest([data, replaceForCardio])); 
+        const rest = Object.fromEntries(formData.entries())["restTime"];
+        const cardio = Object.fromEntries(formData.entries())["cardio"];
+
+        // Replace for cardio only when true
+        var replaceForCardio = (cardio == "Yes");
+
+        store.dispatch(updateRest([rest, replaceForCardio])); 
         unshow(); 
-        // data validation -- non negative numbers?   
     }
 
     return (
@@ -33,72 +41,104 @@ const CogModal = ({show, unshow}) => {
                     onSubmit={handleSubmit}
                     autoComplete="off"
                     className="cog-modal__container">
-                    
-                    <div className="cog-modal__container__parent1">
-                        {/* EDITING REST TIME */}
                         <label 
                             htmlFor="pname"
-                            className='cog-modal__container__parent1__rest-text'>
-                            Edit desired rest time:
+                            className='cog-modal__container__rest-text'>
+                            Select desired rest time:
                         </label>
+                        <div className='cog-modal__container__rest-text__input'>
                             <input 
-                                id="pname"
-                                name="restTime"
-                                className='cog-modal__container__parent1__input'
-                                placeholder="90" // HERE WE NEED THE CURRENT REST TIME PASSED IN AS INPUT
-                                autoFocus
-                                // optional
+                                type="radio"
+                                id="30sec"
+                                name="rest"
+                                // className='cog-modal__container__rest-text__rest-text'
+                                value="30"
                                 />
-                            <p className='cog-modal__container__parent1__seconds'> sec </p>
-                    </div>
+                            <label
+                                // className='cog-modal__container__rest-text__rest-text'
+                                for = "30sec">
+                                    30 sec
+                                </label>
 
-                        {/* EDITING REST SET TIME
-                        <div className="cog-modal__container__parent2">
+                            <input 
+                                type="radio"
+                                id="60sec"
+                                name="rest"
+                                // className='cog-modal__container__rest-text__rest-text'
+                                value="60"
+                                />
+                            <label
+                                // className='cog-modal__container__rest-text__rest-text'
+                                for = "60sec">
+                                    60 sec
+                                    
+                                </label>
+
+                            <input 
+                                type="radio"
+                                id="90sec"
+                                name="rest"
+                                // className='cog-modal__container__rest-text__rest-text'
+                                value="90"
+                                />
+                            <label
+                                // className='cog-modal__container__rest-text__rest-text'
+                                for = "90sec">
+                                    90 sec
+                                </label>
+
+                            <input 
+                                type="radio"
+                                id="120sec"
+                                name="rest"
+                                // className='cog-modal__container__rest-text__rest-text'
+                                value="120"
+                                />
+                            <label
+                                // className='cog-modal__container__rest-text__rest-text'
+                                for = "120sec">
+                                    120 sec
+                                </label>
+                        </div>
+
                         <label 
                             htmlFor="pname"
-                            className='cog-modal__container__parent2__set-text'>
-                            Edit desired rest time between sets:
-                        </label>
+                            className='cog-modal__container__cardio-text'>
+                            Replace mid-workout rest with cardio                        
+                            </label>
+                        <div className='cog-modal__container__cardio-text__input'>
                             <input 
-                                id="pname"
-                                name="restTime"
-                                className='cog-modal__container__parent2__input'
-                                placeholder="20" // HERE WE NEED THE CURRENT REST TIME PASSED IN AS INPUT
-                                autoFocus
-                                optional
+                                type="radio"
+                                id="yes"
+                                name="cardio"
+                                value="Yes"
                                 />
-                            <p className='cog-modal__container__parent2__seconds'> sec </p>
-                        </div> */}
+                            <label
+                                for = "yes">
+                                    Yes
+                                </label>
 
-                        {/* CARDIO TOGGLE */}
-                        {/* <label 
-                            htmlFor="pname"
-                            className='cog-modal__cardio-text'>
-                            Replace some rests with cardio:
-                        </label>
-                        <div className="cog-modal__parent3">
-                            {/* ADD THE TOGGLE FIELD HERE}
-                        </div> */}
+                            <input 
+                                type="radio"
+                                id="no"
+                                name="cardio"
+                                value="No"
+                                />
+                            <label
+                                for = "no">
+                                    No
+                                </label>
+                        </div>
 
-
-                    {/* Deal with errors here */}
-                    {/* <p 
-                        id="msg"
-                        className='cog-modal__error'>
-                        Please enter a positive value
-                    </p> */}
-                    
-                    
-                    {/* Save */}
                     <button 
                         type="submit"
-                        className='cog-modal__button'>
-                        <p className='cog-modal__buttontext'>
+                        className='save-modal__button'>
+                        <p className='save-modal__buttontext'>
                             Save
                         </p>
                     </button>
                 </form>
-            </Modal.Body>
+                </Modal.Body>
         </Modal>
         </>
     )
