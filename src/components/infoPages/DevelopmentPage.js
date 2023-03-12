28e155e7da1cd5d5c276b42eda31d5a557daa7ee
Adela Viskova <av1322@imperial.ac.uk>
2023-03-12 16:06:22 +0000

    Change to github repository on dev page

--- src/components/infoPages/DevelopmentPage.js
+++ src/components/infoPages/DevelopmentPage.js
@@ -23,13 +23,13 @@ const Development = () => {
             <div className='dev__description'>
                 <div className='dev__description-text'>
                     <p> Our application is open source and we welcome contributions from the community.  </p>
-                    <p>If you're interested in contributing, please check out our GitLab repository and follow the instructions in the README file to get started. 
+                    <p>If you're interested in contributing, please check out our GitHub repository and follow the instructions in the README file to get started. 
                     We're always looking for help with new features, with updates to the algorithm behind the workout generation or with updating our video database.</p>
                     <p>Join us in building something great!</p>
                 </div>
-                <div className='dev__gitlab-div'>
-                    <a className='dev__gitlab-link' href="https://gitlab.doc.ic.ac.uk/g227004210/trx-exercise-app" target="_blank" rel="noopener noreferrer">
-                        GitLab Repository
+                <div className='dev__github-div'>
+                    <a className='dev__github-link' href="https://gitlab.doc.ic.ac.uk/g227004210/trx-exercise-app" target="_blank" rel="noopener noreferrer">
+                        GitHub Repository
                     </a>
                 </div>
             </div>
