a5508fd088de8a0cf436652560295e7800332bc7
PhilippePH <55930314+PhilippePH@users.noreply.github.com>
2023-03-12 15:55:10 +0000

    prompt should appear on iphone, and native one on android

--- src/prompt/installPWA.js
+++ src/prompt/installPWA.js
@@ -1,23 +1,13 @@
-// Source to the guide used: https://dev.to/philhardwick/prompt-to-install-a-pwa-on-ios-and-android-with-react-hooks-dkf
-
 import React from 'react';
 import { Button, Modal, Card, CardText, CardBody, CardTitle } from 'reactstrap';
 import useIosInstallPrompt from './isIOS';
-import useWebInstallPrompt from './notIOS';
-
 
 const InstallPWA = () => {
   const [iosInstallPrompt, handleIOSInstallDeclined] = useIosInstallPrompt();
-  const [installPromptEvent, handleInstallDeclined, handleInstallAccepted] = useWebInstallPrompt();
-  console.log(iosInstallPrompt);
-  console.log(installPromptEvent);
-  if (!iosInstallPrompt && !installPromptEvent) {
+  
+  if (!iosInstallPrompt) {
     return null;
   }
-
-  console.log("TOYOYOY");
-  console.log(iosInstallPrompt);
-  console.log(installPromptEvent);
 
   return (
     <Modal isOpen centered>
@@ -30,17 +20,6 @@ const InstallPWA = () => {
           <CardTitle className="text-center">
             <h3>Install App</h3>
           </CardTitle>
-          {installPromptEvent && (
-            <>
-            <div className="d-flex justify-content-around">
-              <Button color="primary" onClick={handleInstallAccepted}>
-                Install
-              </Button>
-              <Button onClick={handleInstallDeclined}>Close</Button>
-            </div>
-            </>
-          )}
-
           {iosInstallPrompt && (
             <>
               <CardText className="text-center">
@@ -59,7 +38,6 @@ const InstallPWA = () => {
               </div>
             </>
           )}
-          
         </CardBody>
       </Card>
     </Modal>
