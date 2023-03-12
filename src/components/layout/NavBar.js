7caaeb4f6fd11e31567ed0982edcbb37f8d92f28
Eugene Ting <eugeneting@ymail.com>
2023-03-12 16:40:45 +0000

    Refactored modal button, navbar inline style

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -12,26 +12,13 @@ const NavBar = () => {
     return (
         <div 
             id="navbar-container"
-            style={{
-                position: "sticky",
-                top: "0px",
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-                backgroundColor: "white",
-                justifyContent: "space-between",
-                borderBottom: "2px solid black",
-                paddingTop: "1rem",
-                paddingBottom: "0.5rem"
-            }}
-        >
+            className="nav-bar__container">
             <div 
                 id="navbar-logo" 
                 onClick={() => { navigate("/") }}
                 style={{
                     cursor: "pointer"
-                }}
-            >
+                }}>
                 <img
                     style={{
                         paddingLeft: "1rem",
@@ -39,31 +26,12 @@ const NavBar = () => {
                         width: "3.5rem"
                     }}
                     src={"/logo.png"}
-                    alt={"logo"}
-                />
+                    alt={"logo"}/>
             </div>
             <div className = 'nav-bar__links-div'>
                 <div className = 'nav-bar__link' onClick={() => { navigate("/about")}} > About </div>
                 <div className = 'nav-bar__link'onClick={() => { navigate("/development")}}>Development</div>
             </div>
-            {/* <div 
-                id="navbar-home" 
-                onClick={() => { navigate("/") }}
-                style={{
-                    paddingRight: 15,
-                    cursor: "pointer"
-                }}
-            >
-                <GrHomeRounded
-                    style={{
-                        fontSize: "30px",
-                        "&:hover": {
-                            backgroundColor: "red",
-                            color: "green"
-                          },
-                    }}
-                />
-            </div>   */}
         </div>
     )
 }
